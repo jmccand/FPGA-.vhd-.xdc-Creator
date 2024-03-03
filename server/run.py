4bdc2b135780efa67d83cdb17c473b2cf5c4c272
Joel McCandless <mail@joelmccandless.com>
2024-03-02 19:44:15 -0500

    serve React from Flask

--- server/run.py
+++ server/run.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return app.send_static_file('index.html')  # Return index.html from the static folder
 
 if __name__ == '__main__':
     app.run(debug=True,port=3002)

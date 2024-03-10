90c45d34d61aa33285ff8031d4d3a22c09a52a3d
Joel McCandless <mail@joelmccandless.com>
2024-03-09 22:57:33 -0500

    create Form.js file and render it

---
+++ client/src/Form.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Form() {
+  const [name, setName] = useState("");
+
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Form />);
+
+export default Form;

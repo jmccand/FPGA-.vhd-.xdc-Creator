410db62fe03aab5483c977174577066a52f51a07
Joel McCandless <mail@joelmccandless.com>
2024-07-02 23:15:33 -0700

    refactor code, add .css file

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,102 +1,105 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
 import { useState } from 'react';
 import 'reactjs-popup/dist/index.css';
 import ReactDOM from 'react-dom/client';
+import './Form.css';
+import $ from 'jquery';
 
 function Form() {
-    // files and selected file
-    const [files, setFiles] = useState(["constraints.xdc"]);
-    const [selectedFile, setSelectedFile] = useState("");
-    const [popupDisplayed, setPopupDisplayed] = useState(false);
-	const [newFilename, setNewFilename] = useState('');
- 
+	// file list
+    const [files, setFiles] = useState(['constraints.xdc']);
+	// selected file
+    const [selectedFile, setSelectedFile] = useState('');
+	// popup displayed
+    const [popupDisplayed, setPopupDisplayed] = useState(false); 
+
+	// modal for creating a file
 	function CreateFileModal() {
 		return <>
-				<div style={{display: 'flex',
-					'flex-direction': 'column',
-
-					display: selectedFile === 'new' ? 'default' : 'none'}}>
-					<div style={{
-						display: 'flex',
-						'flex-direction': 'row',
-						'justify-content': 'end'}}>
-						<div style={{display: 'flex'}}>
+				<div className={ `modal ${selectedFile === 'new' ? 'open' : 'closed'}` }>
+					<div className='topbar'>
+						<div style={{display: 'flex', paddingRight: '0.5rem', cursor: 'pointer'}} onClick={() => setSelectedFile(files[0])}>
 							X
 						</div>
 					</div>
-					<div style={{display: 'flex'}}>
-						Filename: <input onChange={updateFilename} value={newFilename}/><button type="submit" onClick={createFile}>create file</button>
+					<div className='content'>
+						<span style={{paddingRight: '0.5rem'}}>Filename:</span><input type='text' name='filename' id='newFilenameInput'/><button type="submit" onClick={createFile}>create file</button>
 					</div>
 				</div>
 			</>
 	}
 
+	// create file on modal submissiond=
 	const createFile = () => {
+		var newFilename = document.getElementById('newFilenameInput').value
 		if (!newFilename.endsWith(".vhd")) {
-			if (newFilename.indexOf(".") != -1) {
+			if (newFilename.indexOf(".") !== -1) {
 				newFilename = newFilename.substring(0, newFilename.indexOf("."));
 			}
 			newFilename += ".vhd";
 		}
-		console.log("adding filename " + newFilename);
 		setFiles([...files, newFilename]);
 		setPopupDisplayed(false);
 	}
 
-    const handleSelectFile = (event) => {
-		console.log(event.target.value + ' selected!');
-		setSelectedFile(event.target.value);
-    }
-
-    const updateFilename = (event) => {
-		newFilename = event.target.value;
-    }
-    
+	// form content
     return (
 		<>
 			<CreateFileModal></CreateFileModal>
-			<form>
-				<label for='fileSelectInput' style={{'padding-right': '1rem'}}>
-				FILE:
-				</label>
-				<SelectAndAdd files={files} selectedFile={selectedFile} handleSelectFile={handleSelectFile}></SelectAndAdd>
-				<br />
-				TYPE:
-				<select>
-				<option value="functional">functional</option>
-				<option value="structural">structural</option>
-				<option value="procedural">procedural</option>
-				</select>
+			<form style={{position: 'absolute',
+				left: '50%',
+				transform: 'translateX(-50%)',
+				top: '10%',
+				border: '2px solid black',
+				borderRadius: '1rem',
+				padding: '1rem',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '1rem'}}>
+					<div style={{display: 'flex'}}>
+						<label htmlFor='fileSelectInput'>
+							FILE:
+						</label>
+						<SelectAndAdd></SelectAndAdd>
+					</div>
+					<div style={{display: selectedFile.endsWith('.vhd') ? 'flex' : 'none'}}>
+						<label htmlFor='fileTypeInput'>
+							TYPE:
+						</label>
+						<select id='fileTypeInput'>
+							<option value="functional">functional</option>
+							<option value="structural">structural</option>
+							<option value="procedural">procedural</option>
+						</select>
+					</div>
 				<IOMapping></IOMapping>
 			</form>
 		</>
     )
-}
 
-function SelectAndAdd({files, selectedFile, handleSelectFile}) {
-    return (
-	    <select id='fileSelectInput' value={selectedFile} onChange={handleSelectFile}>
-	    { files.map(filename => <option key={filename} value={filename}>{filename}</option>) }
-	    <option value="new">NEW</option>
-	    </select>
-    )
-}
+	// the select object with all the files as options
+	function SelectAndAdd() {
+		return (
+			<select id='fileSelectInput' value={selectedFile} onChange={(event) => setSelectedFile(event.target.value)}>
+			{ files.map(filename => <option key={filename} value={filename}>{filename}</option>) }
+			<option value="new">NEW</option>
+			</select>
+		)
+	}
 
-function IOMapping({}) {
-    return (
-	    <table>
-	    <tbody>
-	    <tr>
-	    <td>NAME</td>
-	    <td>TYPE</td>
-	    </tr>
-	    </tbody>
-	    </table>
-    )
+	// the IOMapping
+	function IOMapping({}) {
+		return (
+			<table>
+			<tbody>
+			<tr>
+			<td>NAME</td>
+			<td>TYPE</td>
+			</tr>
+			</tbody>
+			</table>
+		)
+	}
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Form />);
 
 export default Form;
